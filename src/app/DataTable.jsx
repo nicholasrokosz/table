ba39a8cad22bf615df5a81637b415abb93ba8692
nickrokosz <nickrokosz@runsafe.com>
2025-06-01 23:44:48 -0500

    Add basic search for name and location columns

--- src/app/DataTable.jsx
+++ src/app/DataTable.jsx
@@ -1,13 +1,47 @@
+"use client";
+
+import { useState } from "react";
 import { addRow } from "./lib/data";
 import SystemTableRow from "./SystemTableRow";
 
 export default function DataTable({ data, columns, title }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchedData, setSearchedData] = useState(data);
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+
+    if (!e.target.value) {
+      setSearchedData(data);
+    } else {
+      // basic search for name and location columns
+      const filteredData = data.filter(({ name, location }) => {
+        const normalizedName = name?.toLowerCase().trim();
+        const normalizedLocation = location?.toLowerCase().trim();
+        const normalizedSearchTerm = e.target.value.toLowerCase().trim();
+
+        return (
+          (normalizedName && normalizedName.includes(normalizedSearchTerm)) ||
+          (normalizedLocation &&
+            normalizedLocation.includes(normalizedSearchTerm))
+        );
+      });
+
+      setSearchedData(filteredData);
+    }
+  };
+
   return (
     <div className="data-table">
       <div className="title-row">
         <h1>{title}</h1>
         <div>
-          <input className="search-bar" placeholder="Search" />
+          <input
+            className="search-bar"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={handleSearch}
+          />
           <button onClick={addRow}>Add New System</button>
         </div>
       </div>
@@ -21,14 +55,14 @@ export default function DataTable({ data, columns, title }) {
           </tr>
         </thead>
         <tbody>
-          {data.length === 0 ? (
+          {searchedData.length === 0 ? (
             <tr>
               <td className="empty-message" colSpan={columns.length + 1}>
                 No Data Available
               </td>
             </tr>
           ) : (
-            data.map((row) => <SystemTableRow row={row} key={row.id} />)
+            searchedData.map((row) => <SystemTableRow row={row} key={row.id} />)
           )}
         </tbody>
       </table>
